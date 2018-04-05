38bc88dd0dc96d8636a669044fd40fa9486ff710
zhongsiran <zhongsiran@gmail.com>
2018-04-05 15:44:06 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,13 @@
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <dir class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="{{route('home')}}" id="logo">Sample App</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{route('help')}}">help</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </nav>
+        </div>
+    </dir>
+</header>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>@yield('title', 'Sample')</title>
-  </head>
-  <body>
-    @yield('content')
-  </body>
+    <head>
+        <title>@yield('title', 'Sample App') - Laravel tutorial)</title>
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+    </head>
+    <body>
+        @include('layouts._header')
+
+        <div class="container">
+            <div class="col-md-offset-1 col-md-10">
+                @yield('content')
+                @include('layouts._footer')
+            </div>
+        </div>
+    </body>
 </html>
