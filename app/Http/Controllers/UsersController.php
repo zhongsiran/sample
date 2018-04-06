c886b4a8322b62ef8d894bcc352be1343272a42d
zhongsiran <zhongsiran@gmail.com>
2018-04-06 08:29:21 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,4 +18,22 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', 'welcome, a new journey has begun.');
+        return redirect()->route('users.show', [$user]);
+    }
+
 }
