a9d8cc17c90419ceb10484ce9c8f10114743fd37
zhongsiran <zhongsiran@gmail.com>
2018-04-06 09:17:53 +0000

    env db url

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -6,7 +6,7 @@ function get_db_config()
         $url = parse_url(getenv("DATABASE_URL"));
 
         return $db_config = [
-            'connetcion' => 'pgsql',
+            'connection' => 'pgsql',
             'host' => $url["host"],
             'database' => substr($url["path"], 1),
             'username' => $url["user"],
